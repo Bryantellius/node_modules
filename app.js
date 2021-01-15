3909587d9cb57595f17176f4a5ac0f3fb21a99ae
Ben <bbryant@truecoders.io>
2021-01-15 17:19:06 -0600

    Added solution for Answer branch

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+const { myInfo } = require("./mymodule");
+
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log("EXERCISE:\n==========\n");
 
 const http = require("http");
+const port = 3000;
 
+console.log(myInfo);
 // Finish setting up the server
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    let info = myInfo();
+    res.end(
+      "I am " +
+        info.name +
+        ", aged " +
+        info.age +
+        ", living in " +
+        info.city +
+        "."
+    );
+  })
+  .listen(port, () => console.log("Server listening on port: " + port));
---
+++ mymodule.js
@@ -0,0 +1,7 @@
+exports.myInfo = function () {
+  return {
+    name: "Ben",
+    age: 23,
+    city: "Birmingham",
+  };
+};
